2ff7f4c9d9afb3844bebd0534b451385b95d4458
mohamed husein panjwani <91213265+husein-panjwani@users.noreply.github.com>
2024-03-11 01:45:04 +0530

    Update create_table_in_diffrent_file.py
    Multiplication Tables Generator
    
    Description:
    This Python script generates multiplication tables from 2 to 20 and saves them in individual text files within a 'tables' directory. Each file is named based on the multiplier, and the content includes the multiplication expressions and their results.
    
    Usage:
    
    Ensure Python is installed on your system.
    Run the script.
    The 'tables' directory will be created (if not exists), and individual files for each multiplication table will be generated.
    
    Note:
    If encountering issues, check permissions and adjust the file paths as needed.
    Author:
    Mohamed husein panjwani

--- learning python journey/Learn File IO /create_table_in_diffrent_file.py
+++ learning python journey/Learn File IO /create_table_in_diffrent_file.py
@@ -1,1 +1,10 @@
+import os
+#Ensure the 'tables' directory exists
+tables_dir = "enter you relative path "
+os.makedirs(tables_dir, exist_ok=True)
+
+for i in range(2, 21):
+    with open(f"{tables_dir}/multiplication_table_of_{i}.txt", 'w') as f:
+        for j in range(1, 11):
+            f.write(f"{i} X {j} = {i*j}\n")
 
